9a12a53e6400161a6f00924ea45ebf80d4acf22a
Lunnariannaa <couture.culture.pro@gmail.com>
2025-01-13 00:59:06 +0100

    bou

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
@@ -7,7 +9,7 @@ chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 
-service = Service("C:\big script\chromedriver.exe")
+service = Service(r"C:\script_factures\chromedriver.exe")
 
 driver = webdriver.Chrome(service=service, options=chrome_options)
 
@@ -19,7 +21,7 @@ try:
     driver.get(site_url)
     time.sleep(2)
 
-    driver.find_element(By.ID, "login_field").send_keys(username)
+    driver.find_element(By.NAME, "username").send_keys(username)
     driver.find_element(By.ID, "password").send_keys(password)
     driver.find_element(By.NAME, "commit").click()
 
