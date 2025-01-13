ec8012305b6ea36e20983f644ef5344b9a47f6da
Lunnariannaa <couture.culture.pro@gmail.com>
2025-01-13 22:27:35 +0100

    bouh

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 
-service = Service(r"C:\script_factures\chromedriver.exe")
+service = Service(r"C:\script_factures\chromedriver")
 
 driver = webdriver.Chrome(service=service, options=chrome_options)
 
